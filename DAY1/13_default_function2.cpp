ca8f52aead70cb5d6ae8bbac849415f76384a81f
ecourse <smkang@codenuri.co.kr>
2022-12-12 14:48:11 +0900

    Add files via upload

--- DAY1/13_default_function2.cpp
+++ DAY1/13_default_function2.cpp
@@ -7,7 +7,8 @@ class Point
 public:
 	int x, y;
 
-	Point() {}
+//	Point() {}
+	Point() = default;
 };
 
 int main()
@@ -15,6 +16,7 @@ int main()
 	Point pt1;		// 모든 멤버(x,y)가 쓰레기값을 가지게 됩니다.
 	Point pt2{};	// value initialization(내일 자세히..)
 					// => 모든 멤버를 0으로 초기화
+					// => 단, 사용자가 생성자 구현을 제공하지 않을때
 
 	std::cout << pt2.x << std::endl;
 	std::cout << pt2.y << std::endl;
