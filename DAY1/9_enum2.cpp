070aaeb09f229811e0576377e046112665c9acee
ecourse <smkang@codenuri.co.kr>
2022-12-12 13:40:12 +0900

    Add files via upload

---
+++ DAY1/9_enum2.cpp
@@ -0,0 +1,26 @@
+
+// C++98 enum
+// enum DAYOFWEEK { sum = 0, mon = 1 };
+
+// C++11 의 새로운 enum
+enum class DAYOFWEEK { sum = 0, mon = 1 };
+
+//void foo(int dayofaweek) {} // 모든 정수를 받을수 있게 된다.
+
+void foo(DAYOFWEEK dayofaweek) {} // 요일만 받을수 있다.
+
+int main()
+{
+//	int n1 = mon; // error. DAYOFWEEK 필요
+//	int n2 = DAYOFWEEK::mon;	// error.
+								// int 가 아닌 DAYOFWEEK 타입
+
+	DAYOFWEEK n3 = DAYOFWEEK::mon; // ok
+
+	foo(DAYOFWEEK::mon);
+}
+
+
+
+
+
