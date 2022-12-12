202a8c8ff13a779e61f5478b11d82fead3ca279f
ecourse <smkang@codenuri.co.kr>
2022-12-12 14:45:54 +0900

    Add files via upload

---
+++ DAY1/13_default_function2.cpp
@@ -0,0 +1,26 @@
+// 13_default_function - 41 page
+#include <type_traits>
+#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point() {}
+};
+
+int main()
+{	
+	Point pt1;		// 모든 멤버(x,y)가 쓰레기값을 가지게 됩니다.
+	Point pt2{};	// value initialization(내일 자세히..)
+					// => 모든 멤버를 0으로 초기화
+
+	std::cout << pt2.x << std::endl;
+	std::cout << pt2.y << std::endl;
+
+
+}
+
+
+
