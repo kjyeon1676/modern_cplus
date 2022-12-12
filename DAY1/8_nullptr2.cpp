113b5ce60472ee5a07619482ab1775d182c6d22c
ecourse <smkang@codenuri.co.kr>
2022-12-12 12:00:01 +0900

    Add files via upload

--- DAY1/8_nullptr2.cpp
+++ DAY1/8_nullptr2.cpp
@@ -6,15 +6,18 @@ void foo(int* p)
 }
 
 template<typename T>
-void forward_foo(T arg)
+void forward_foo(T arg)	// int arg = 0 로 T가 결정됩니다.
 {
-	foo(arg);
+	foo(arg);			// foo(arg)
 }
 
 int main()
 {
 	foo(0); //ok
-	forward_foo(0); // error 왜 에러인지 생각해 보세요
+
+//	forward_foo(0); // error 왜 에러인지 생각해 보세요
+	
+	forward_foo(nullptr); // ok.
 }
 
 
