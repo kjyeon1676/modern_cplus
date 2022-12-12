0328a738d8346a45fec9d6fb4ed30bbfd8aee7ce
ecourse <smkang@codenuri.co.kr>
2022-12-12 09:44:48 +0900

    Add files via upload

--- DAY1/1_static_assert1.cpp
+++ DAY1/1_static_assert1.cpp
@@ -5,13 +5,21 @@
 //				   컴파일 시간의 유효성 조사
 //				   키워드. include 필요 없음
 
-static_assert(sizeof(int*) >= 8, "error, not 64bit");
+// 주의 1. 표현식은 반드시 컴파일시간에 조사가능해야 합니다.
+// 2. 함수 안 또는 밖에 만들수 있습니다.
+static_assert( sizeof(int*) >= 8, "error, not 64bit");
+
 
 void foo(int age)
 {
 	// 함수 인자를 바로 사용하지 말고.. 
 	// 인자값의 유효성을 확인하는 것이 안전합니다.
 	assert( age > 0 );
+
+	// 아래 처럼 할수 없습니다.
+	// 변수값을 컴파일시간에 조사하는 것은 안됩니다.
+	static_assert(age > 0, "error, not 64bit");
+
 
 	// age > 0 이라고 가정하고 수십줄의 코드 작성
 	int* p = new int[age];
