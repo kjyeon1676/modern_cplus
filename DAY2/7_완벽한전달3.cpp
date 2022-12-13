0e8e3776d662e83f4abd9ea27a42c934dd39d8fb
ecourse <smkang@codenuri.co.kr>
2022-12-13 17:02:56 +0900

    Add files via upload

---
+++ DAY2/7_완벽한전달3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void hoo(int&& arg) {}
+
+template<typename F> void chronometry(F f, int& arg)
+{
+	f(arg);
+}
+template<typename F> void chronometry(F f, int&& arg)
+{
+	f(arg);
+}
+
+int main()
+{
+	hoo(10); // ok
+	chronometry(hoo, 10); // error.. 왜??? 에러일까요 ?
+}
