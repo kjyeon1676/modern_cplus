03a6b9b3e8ee4a9d640811417a7a87cfba2748d5
ecourse <smkang@codenuri.co.kr>
2022-12-13 10:19:33 +0900

    Add files via upload

--- DAY2/1_초기화12.cpp
+++ DAY2/1_초기화12.cpp
@@ -1,7 +1,16 @@
 #include <vector>
 
+template<typename T, int N> struct array
+{
+	T buff[N];
+	
+	// array 는 반드시 = {} 초기화를 지원해야 합니다.
+	// 1. std::initializer_list 가지는 생성자 제공하거나
+	// 2. 어떠한 생성자도 만들지 말거나(+가상함수도)
+};
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 }; // 모든 요소가 힙에!!
-	std::array<int, 5> arr = { 1,2,3,4,5 }; // 모든 요소가 스택에 .
+	array<int, 5> arr = { 1,2,3,4,5 }; // 모든 요소가 스택에 .
 }
