cb72aece2a2dfaa530c61bbfb0d4381155843f51
ecourse <smkang@codenuri.co.kr>
2022-12-13 12:08:27 +0900

    Add files via upload

---
+++ DAY2/2_타입추론12.cpp
@@ -0,0 +1,16 @@
+// 2_타입추론12
+
+template<typename T>
+void f1(T a, T b)
+{
+}
+template<typename T>
+void f2(T& a, T& b)
+{
+}
+
+int main()
+{
+	f1("orange", "apple"); // ok
+	f2("orange", "apple"); // error. 왜 에러일까요 ?
+}
