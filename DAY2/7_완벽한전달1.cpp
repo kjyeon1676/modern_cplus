d3d944643602dc9bcd5792ef4b547dcd5098ef0a
ecourse <smkang@codenuri.co.kr>
2022-12-13 16:49:11 +0900

    Add files via upload

---
+++ DAY2/7_완벽한전달1.cpp
@@ -0,0 +1,24 @@
+// 7_완벽한전달1 - 113 page
+#include <iostream>
+
+void foo(int a)  {}
+void goo(int& a) { a = 100; }
+
+// 인자로 전달된 함수의 성능(수행시간)을 측정하는 함수
+template<typename F, typename T>
+void chronometry(F f, T arg)
+{
+	// 시간을 구한후 보관
+	f(arg);	// 함수 호출
+	// 다시 시간을 구한후, 소요시간 출력
+}
+
+int main()
+{
+	int n = 10;
+
+	chronometry(foo, 10); // foo(10);
+	chronometry(goo, n);  // goo(n);
+
+	std::cout << n << std::endl;
+}
