cb8b850fd22834b51a10a110b74d9ded9825a0ae
ecourse <smkang@codenuri.co.kr>
2022-12-13 15:54:17 +0900

    Add files via upload

--- DAY2/6_forwarding_reference3.cpp
+++ DAY2/6_forwarding_reference3.cpp
@@ -6,7 +6,7 @@ void f2(int&& r) {}
 // int& : int 타입의 lvalue 만 받겠다는 것
 // int&&: int 타입의 rvalue 만 받겠다는 것
 // 
-// T& : ?
+// T& : 임의 타입의 lvalue 만 받겠다는 의미
 
 
 template<typename T> void f3(T& a)
@@ -18,9 +18,15 @@ int main()
 	int n = 5;
 
 	// 1. 사용자가 타입을 명시적으로 전달하는 경우
-	f3<int>(? );	// T=?		T&=?		생성된함수:f3(? a)
-	f3<int&>(? );	// T=?		T&=?		생성된함수:f3(? a)
-	f3<int&&>(? );	// T=?		T&=?		생성된함수:f3(? a)
+	f3<int>(n );	// T=int	T&=int&		생성된함수:f3(int& a)
+	f3<int&>(n );	// T=int&	T&=int& &	생성된함수:f3(int& a)
+	f3<int&&>(n );	// T=int&&	T&=int&& &	생성된함수:f3(int& a)
+
+	// 2. 사용자가 타입을 전달하지 않으면 
+	// => 컴파일러는 함수 인자를 보고 함수를 생성하려고 노력합니다.
+	f3(10);	// T=int, int&, int&& 등으로 해도 모두 10을 받을수 없다
+			// 컴파일 에러!
+	f3(n);	// T=int 로 결정.!
 }
 
 
