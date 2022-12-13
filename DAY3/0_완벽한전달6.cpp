037882742f91804d329222b38d66e032b46c0609
ecourse <smkang@codenuri.co.kr>
2022-12-14 08:43:36 +0900

    Add files via upload

---
+++ DAY3/0_완벽한전달6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void foo(int a) {}
+void goo(int& a) { a = 100; }
+
+void hoo(int&& arg) {}
+
+/*
+template<typename F>
+void chronometry(F f, int& arg)
+{
+//	f(arg);
+	f( static_cast<int&>(arg) ); // 필요없는 캐스팅이지만
+								 // 있어도 전혀 문제가 되지 않습니다.
+}
+template<typename F>
+void chronometry(F f, int&& arg)
+{
+	f(static_cast<int&&>(arg));
+}
+*/
+
+template<typename F, typename T>
+void chronometry(F f, T&& arg)
+{
+	f(static_cast<T&&>(arg));
+}
+
+int main()
+{
+	int n = 0;
+	chronometry(foo, 10);
+	chronometry(goo, n);
+}
