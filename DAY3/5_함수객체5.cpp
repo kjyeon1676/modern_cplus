7e236bea52be150dd497e07f3775e22012a796be
ecourse <smkang@codenuri.co.kr>
2022-12-14 16:14:23 +0900

    Add files via upload

--- DAY3/5_함수객체5.cpp
+++ DAY3/5_함수객체5.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <algorithm>
 
+// 아래와 유사하게 만들어진 것이 C언어의 "qsort" 입니다
+// => 결국 비교 함수를 다시 호출하게 되므로 느립니다.
+// => 데이타량이 많은 경우는 사용하지 마세요..
+
 void Sort(int* arr, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
@@ -10,15 +14,14 @@ void Sort(int* arr, int sz, bool(*cmp)(int, int) )
 		{
 		//	if (arr[i] > arr[j])
 
-
 			if( cmp(arr[i], arr[j]) == true )
 				std::swap(arr[i], arr[j]);
 		}
 	}
 }
 //----------------------------------
-bool cmp1(int a, int b) { return a > b; }
-bool cmp2(int a, int b) { return a < b; }
+inline bool cmp1(int a, int b) { return a > b; }
+inline bool cmp2(int a, int b) { return a < b; }
 
 int main()
 {
