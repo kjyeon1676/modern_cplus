1bf591117f29c90a7d5e2ebe2140e1d97a585517
ecourse <smkang@codenuri.co.kr>
2022-12-14 13:58:44 +0900

    Add files via upload

---
+++ DAY3/3_move15.cpp
@@ -0,0 +1,23 @@
+// 3_move15 - 10¹ø º¹»ç
+#include <iostream>
+#include <vector>
+
+class Object
+{
+public:
+	Object() {}
+	Object(const Object&) { std::cout << "copy" << std::endl; }
+
+	Object(Object&&) noexcept
+	{
+		std::cout << "move" << std::endl;
+	}
+};
+int main()
+{
+	Object o1;
+	Object o2 = o1; 
+	Object o3 = std::move(o1); 
+
+}
+
