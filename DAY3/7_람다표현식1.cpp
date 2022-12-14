13acdd9e0da9c8c20eeeecbe9dabe2f424cedb6b
ecourse <smkang@codenuri.co.kr>
2022-12-14 17:06:51 +0900

    Add files via upload

---
+++ DAY3/7_람다표현식1.cpp
@@ -0,0 +1,36 @@
+// 5_람다표현식1
+#include <iostream>
+#include <algorithm>
+#include <functional> // 이 안에 C++ 표준에 less<> 와 greater<> 가 있습니다.
+					  // C++98
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+	
+	// sort 하고 싶다면
+	
+	// 1. 비교 정책을 전달하지 않으면 - 오름 차순 정렬
+	// std::sort(v.begin(), v.end()); 
+
+	// 2. 비교정책으로 일반함수 사용 - 비교 정책이 인라인 치환 안됨
+	//std::sort(v.begin(), v.end(), cmp1); 
+
+	// 3. 비교정책으로 함수 객체 사용
+	// => 이미 C++ 표준의 <functional> 헤더에 제공됨.(C++98 부터)
+	std::less<int> f;
+	std::sort(v.begin(), v.end(), f);
+	std::sort(v.begin(), v.end(), std::less<int>() ); // 임시객체로 전달
+	std::sort(v.begin(), v.end(), std::less()); // C++17 부터 가능.
+
+
+	// 4. C++11 부터는 람다 표현식 사용가능
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; }); 
+}
+
+
+
+
+
