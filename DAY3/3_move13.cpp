a454561d0c8b5184e58d067f453885557c5b63f5
ecourse <smkang@codenuri.co.kr>
2022-12-14 14:52:37 +0900

    Add files via upload

--- DAY3/3_move13.cpp
+++ DAY3/3_move13.cpp
@@ -12,13 +12,23 @@ private:
 	int age;
 	std::array<int, 32> data;
 public:
+
+	// 인자로 전달받은 문자열을 보관(retain) 하는 경우는
+	// move 지원을 위해 아래 처럼 2개의 setter 가 필요 합니다.
 	void set_name(const std::string& n) { name = n; }
 	void set_name(std::string&& n)      { name = std::move(n); }
 
+
+	// 인자로 전달받은것을 보관하지 않는 경우는
+	// move 지원 등을 고려할필요 없습니다.
 	void print_msg(const std::string& msg) const
 	{
 		std::cout << msg << std::endl;
 	}
+
+	// move의 효과가 없는 타입은 move를 지원할 필요 없습니다.
+	// => 동적할당된 자원이 없는 경우들
+	// => std::array<>, int 등.. 
 	void set_data(const std::array<int, 32>& d)
 	{
 		data = d;
