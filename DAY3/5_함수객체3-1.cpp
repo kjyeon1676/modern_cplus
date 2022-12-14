db1e98f91574c5a7532fd473ec353fb0e6508836
ecourse <smkang@codenuri.co.kr>
2022-12-14 15:55:51 +0900

    Add files via upload

--- DAY3/5_함수객체3-1.cpp
+++ DAY3/5_함수객체3-1.cpp
@@ -8,8 +8,7 @@ bool foo(int n) { return n % 3 == 0; }
 // 함수객체는 "클로져"가 될수 있습니다.
 struct IsDivide
 {
-	int value;
-	
+	int value;	
 	IsDivide(int n) : value(n) {}
 
 	bool operator()(int n) const {	return n % value == 0;	}
@@ -27,8 +26,11 @@ int main()
 					// 흔히, 캡쳐 한다고 표현합니다.
 	int ret = f(6); // 6 % k  즉, 단항함수 인데, 캡쳐된 k값 사용
 
-	auto p1 = std::find_if(v.begin(), v.end(),
-		"foo + 지역변수k");
+	auto p1 = std::find_if(v.begin(), v.end(), f);
+	auto p2 = std::find_if(v.begin(), v.end(), IsDivide(k) );
+			// => IsDivide 라는 단항함수 객체를 전달하는데..
+			// => k라는 지역변수의 값을 같이 전달하는 효과!!
+			// => 문맥이 속한 지역변수를 캡쳐하는 기능을 가진 일급객체
 }
 
 
