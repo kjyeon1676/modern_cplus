646428fcf5bb46fd3e31f50c22bc27ad19d4cd4c
ecourse <smkang@codenuri.co.kr>
2022-12-14 15:15:07 +0900

    Add files via upload

---
+++ DAY3/4_람다표현식2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool foo(int n) { return n % 3 == 0; }
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,5,4,3,7,8 };
+
+	// v에서 3을 찾고 싶다.
+	auto p1 = std::find(v.begin(), v.end(), 3);
+
+	std::cout << *p1 << std::endl;
+
+
+	// v에서 1번째 나오는 "3의 배수"를 찾고 싶다.?
+	
+	// std::find    : 값  검색, 마지막 인자로 "값" 전달
+	// std::find_if : 조건 검색, 마지막 인자로 "함수" 전달
+
+	auto p2 = std::find_if(v.begin(), v.end(), foo ); // C++98 스타일
+
+	auto p3 = std::find_if(v.begin(), v.end(),  ); // C++11 스타일
+
+}
+
+
