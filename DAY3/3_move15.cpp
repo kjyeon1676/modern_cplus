8f966fd7d40f6b0d9416f1b4c96caed1609a0e1b
ecourse <smkang@codenuri.co.kr>
2022-12-14 14:03:48 +0900

    Add files via upload

--- DAY3/3_move15.cpp
+++ DAY3/3_move15.cpp
@@ -15,8 +15,12 @@ public:
 };
 
 // std::move 를 만들어 봅시다.
+// T& : 인자로 lvalue 만 받겠다는 것 
+// T&&: 인자로 lvalue 와 rvalue 를 모두 받겠다는 것
+
+// move 는 lvalue 와 rvalue 를 모두 받을수 있어야 합니다.
 template<typename T>
-T&& xmove(T& obj)
+T&& xmove(T&& obj)
 {
 	return static_cast<T&&>(obj);
 }
@@ -24,8 +28,10 @@ T&& xmove(T& obj)
 int main()
 {
 	Object o1;
-	Object o2 = o1; 
-	Object o3 = xmove(o1); 
+	Object o2 = o1;			// copy
+	Object o3 = xmove(o1);	// move
+
+	Object o3 = xmove(Object()); // ??
 
 }
 
