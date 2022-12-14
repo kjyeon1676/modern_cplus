08f689fe784b059564f5d3afa2089b5e63d4de26
ecourse <smkang@codenuri.co.kr>
2022-12-14 16:09:17 +0900

    Add files via upload

--- DAY3/5_함수객체5.cpp
+++ DAY3/5_함수객체5.cpp
@@ -2,22 +2,34 @@
 #include <vector>
 #include <algorithm>
 
-void Sort(int* arr, int sz)
+void Sort(int* arr, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
 	{
 		for (int j = i + 1; j < sz; j++)
 		{
-			if (arr[i] > arr[j])
+		//	if (arr[i] > arr[j])
+
+
+			if( cmp(arr[i], arr[j]) == true )
 				std::swap(arr[i], arr[j]);
 		}
 	}
 }
+//----------------------------------
+bool cmp1(int a, int b) { return a > b; }
+bool cmp2(int a, int b) { return a < b; }
 
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
-	Sort(x, 10);
+
+//	Sort(x, 10, cmp1);
+	Sort(x, 10, cmp2);
 	
+	for (auto e : x)
+		std::cout << e << ", ";
+
+	std::cout << std::endl;
 }
 
