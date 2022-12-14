aa785ab0901301816c3616f37b52961fb978c955
ecourse <smkang@codenuri.co.kr>
2022-12-14 14:00:14 +0900

    Add files via upload

--- DAY3/3_move15.cpp
+++ DAY3/3_move15.cpp
@@ -13,11 +13,19 @@ public:
 		std::cout << "move" << std::endl;
 	}
 };
+
+// std::move 를 만들어 봅시다.
+template<typename T>
+T&& xmove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+
 int main()
 {
 	Object o1;
 	Object o2 = o1; 
-	Object o3 = std::move(o1); 
+	Object o3 = xmove(o1); 
 
 }
 
