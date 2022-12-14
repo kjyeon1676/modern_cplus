3eed00b6fae12e3c0d5b9f8ea632266e9b455254
ecourse <smkang@codenuri.co.kr>
2022-12-14 09:43:20 +0900

    Add files via upload

--- DAY3/1_완벽한전달활용2.cpp
+++ DAY3/1_완벽한전달활용2.cpp
@@ -23,7 +23,20 @@ int main()
 	// 아래 코드는 동적 메모리를 몇번 할당 할까요 ? 2번
 	// 1. new Point
 	// 2. shared_ptr 생성자에서 관리객체 생성
-	std::shared_ptr<Point> sp1(new Point(1,2));
+	//std::shared_ptr<Point> sp1(new Point(1,2));
+
+	// shared_ptr 을 사용하려면 "std::make_shared" 를 사용해서 만들어라!!
+	// 장점 : sizeof(Point) + sizeof(관리객체) 크기의 메모리를
+	//       한번에 할당해서 메모리 효율성을 좋게 한다.
+	std::shared_ptr<Point> sp2 = std::make_shared<Point>(1, 2);
+						// => sizeof(Point) + sizeof(관리객체)를 한번에 할당
+						// => Point 객체를 1, 2로 초기화
+						// => shared_ptr을 만들어 반환
+
+	// make_shared 도 결국 전달받은 인자 1,2 를 Point 생성자로 다시
+	// 전달해야 합니다.
+	// 1, 2 =========> make_shared ==========> Point(int a, int b)
+	// 완벽한 전달 기술로 만들어야 합니다.
 }
 
 
