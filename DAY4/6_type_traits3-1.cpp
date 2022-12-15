17e027ae69ab80bc56635ba9e5bc0b8f78a2e6b1
ecourse <smkang@codenuri.co.kr>
2022-12-15 14:11:00 +0900

    Add files via upload

--- DAY4/6_type_traits3-1.cpp
+++ DAY4/6_type_traits3-1.cpp
@@ -1,14 +1,14 @@
 // 6_type_traits3-1.     1번 복사해오세요.
 #include <iostream>
+#include <type_traits> // std::is_pointer<> 가 있습니다,
 
 template<typename T> void printv(const T& v)
 {
-	if (T 가 포인터 타입이면)
+	if ( std::is_pointer<T>::value )
 		std::cout << v << " : " << *v << std::endl;
 	else
 		std::cout << v << std::endl;
 }
-
 
 int main()
 {
