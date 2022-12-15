2692b7dd4b1d3c1894e6d1f750caf8d7da3ffb70
ecourse <smkang@codenuri.co.kr>
2022-12-15 13:36:10 +0900

    Add files via upload

--- DAY4/6_type_traits.cpp
+++ DAY4/6_type_traits.cpp
@@ -1,6 +1,8 @@
 // 4_type_traits - 184page
 #include <iostream>
 
+// type traits : 타입 T에 대한 다양한 특성(특질)을 조사하는 기술
+
 template<typename T> void printv(const T& v)
 {
 	if ( T 가 포인터 타입이면 )
@@ -8,6 +10,7 @@ template<typename T> void printv(const T& v)
 	else 
 		std::cout << v << std::endl;
 }
+
 
 int main()
 {
