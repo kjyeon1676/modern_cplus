5f7fa913d66f140892abdde7d3046ed2f25d789a
ecourse <smkang@codenuri.co.kr>
2022-12-16 08:44:28 +0900

    Add files via upload

--- DAY5/1_가변인자템플릿1-1.cpp
+++ DAY5/1_가변인자템플릿1-1.cpp
@@ -1,13 +1,25 @@
 // 1_가변인자템플릿1-1
 
 // 타입 인자가 한개인 함수 템플릿
+// => 인자가 한개인 함수를 생성할때 사용하는 틀.. 
 template<typename T> void foo(T arg)
 {
+}
 
+// 가변 인자 함수 템플릿
+// => ... 의 위치 정확히 알아 두세요..
+// => 인자가 N개인 함수를 생성할수 있는 템플릿
+template<typename ... Types> void goo(Types ... arg)
+{
 }
 
 int main()
 {
-	foo(1);
-	foo(3.4);
+	foo(1);		// foo(int) 생성
+	foo(3.4);	// foo(double) 생성
+
+	goo(1);      // goo(int)
+	goo(3.4);	 // goo(double) 생성
+	goo(1, 3.4); // goo(int, double) 생성
+	goo();		 // goo() 생성..
 }
