9113edae547c4759b34cb730de650f66c7d3ecc4
ecourse <smkang@codenuri.co.kr>
2022-12-15 10:40:24 +0900

    Add files via upload

--- DAY4/3_람다기타2.cpp
+++ DAY4/3_람다기타2.cpp
@@ -18,8 +18,21 @@ int main()
 	// => 일반함수는 C++20 부터 인자로 auto 사용가능합니다.
 	auto f1 = [](auto a, auto b) { return a + b; };
 
-	std::cout << f1(1, 2) << std::endl;
-	std::cout << f1(1.1, 2.1) << std::endl;
-	std::cout << f1(1,   2.1) << std::endl;
+	std::cout << f1(1, 2) << std::endl;		// operator(int, int)생성
+	std::cout << f1(1.1, 2.1) << std::endl; // operator(double, double)생성
+	std::cout << f1(1,   2.1) << std::endl; // operator(int, double)생성
 	std::cout << f1(1.1, 2 ) << std::endl;
 }
+
+// generic 람다 표현식의 원리 
+// => operator() 가 함수 템플릿으로 만들어 지는것
+// => 2개의 인자가 서로 다른 템플릿 파라미터 사용
+class CompilerGeneratedName
+{
+public:
+	template<typename T1, typename T2>
+	inline auto operator()(T1 a, T2 b) const
+	{
+		return a + b;
+	}
+};
