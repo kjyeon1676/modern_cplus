e08459cc18d9d064e14108861299e3174b1b8d70
ecourse <smkang@codenuri.co.kr>
2022-12-15 16:11:42 +0900

    Add files via upload

--- DAY4/9_enable_if2.cpp
+++ DAY4/9_enable_if2.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <type_traits>
 
+// static_assert : 조건을 만족하지 않으면 항상 에러!!
+// 필요한것       : 조건을 만족하지 후보에서 제외할수 없을까 ?
+//					=> 다른 함수가 있다면 다름 함수를 사용할수 있게 된다.
+// 
 // 2 정수의 최대 공약수를 구하는 함수를 생각해 봅시다.
 template<typename T> T gcd(T a, T b)
 {
@@ -9,8 +13,15 @@ template<typename T> T gcd(T a, T b)
 	return 0;
 }
 
+double gcd(double a, double b)
+{
+	std::cout << "double" << std::endl;
+	return 0;
+}
 int main()
 {
 	gcd(6, 9);
-	gcd(3.1,  5.4);
+	gcd(3.1,  5.4);	// double 버전 사용
+	gcd(3.1f, 5.4f);// float 이므로 T 버전 사용.
+					// 정수가 아니므로 error.
 }
