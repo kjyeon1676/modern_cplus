dea6d5b4e8e5d1f2648e8c51937711db221bf339
ecourse <smkang@codenuri.co.kr>
2022-12-15 14:41:17 +0900

    Add files via upload

--- DAY4/6_type_traits4.cpp
+++ DAY4/6_type_traits4.cpp
@@ -7,15 +7,23 @@
 // 2. 변형타입얻기 : xxx<T>::type
 
 // 1. primary template 을 만들고 "using type = T" 를 넣으세요
+
 template<typename T> struct remove_pointer
 {
-	using T = type;
+	using type = T;
 };
-
-
+// 2. 원하는 타입을 얻을수 있도록
+//    부분 특수화 문법을 사용해서 타입을 분할 합니다.
+//    int* => int 와 * 로 분할
+template<typename T> struct remove_pointer<T*>
+{
+	using type = T;
+};
 int main()
 {
 	remove_pointer<int*>::type n2;  // int n2
+
+	std::cout << typeid(n2).name() << std::endl;
 }
 
 
