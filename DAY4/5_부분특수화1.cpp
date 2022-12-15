dec258e6702ef0d24238a1a18861110914f481ae
ecourse <smkang@codenuri.co.kr>
2022-12-15 11:55:33 +0900

    Add files via upload

---
+++ DAY4/5_부분특수화1.cpp
@@ -0,0 +1,31 @@
+// 3_부분특수화1 - 156 p
+#include <iostream>
+
+// primary template
+template<typename T> class Stack
+{
+public:
+	void push(T a) { std::cout << "T" << std::endl; }
+};
+
+// partial sepecialization(부분 특수화, 부분 전문화)
+template<typename T> class Stack<T*>
+{
+public:
+	void push(T* a) { std::cout << "T*" << std::endl; }
+};
+
+// specialization(특수화, 전문화)
+template<> class Stack<char*>
+{
+public:
+	void push(char* a) { std::cout << "char*" << std::endl; }
+};
+
+
+int main()
+{
+	Stack<int>   s1; s1.push(0);
+	Stack<int*>  s2; s2.push(0);
+	Stack<char*> s3; s3.push(0);
+}
