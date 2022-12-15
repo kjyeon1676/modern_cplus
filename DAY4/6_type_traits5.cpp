17184d22a6879468cddf1b13a047ea9dd1e4dec9
ecourse <smkang@codenuri.co.kr>
2022-12-15 14:56:46 +0900

    Add files via upload

--- DAY4/6_type_traits5.cpp
+++ DAY4/6_type_traits5.cpp
@@ -4,6 +4,15 @@
 // C++ 표준에 remove_all_pointer 는 없습니다.
 // remove_pointer 구현 참고해서.. remove_all_pointer 만들어 보세요
 
+template<typename T> struct remove_all_pointer
+{
+	using type = T;
+};
+
+template<typename T> struct remove_all_pointer<T*>
+{
+	using type = typename remove_all_pointer<T>::type;
+};
 
 int main()
 {
