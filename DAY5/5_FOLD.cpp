95268e51493d7b802805583fb0bfe528d31c0b43
ecourse <smkang@codenuri.co.kr>
2022-12-16 10:09:03 +0900

    Add files via upload

--- DAY5/5_FOLD.cpp
+++ DAY5/5_FOLD.cpp
@@ -6,19 +6,26 @@
 //				     이항 연산을 수행 - C++17
 // => 반드시 (  ) 가 필요 합니다.
 
-// (pack이름   op   ... ) : (E1 op ( E2 op E3) )
+// unary right fold  : (pack이름 op   ... )    : (E1 op ( E2 op E3) )
+// unary left  fold  : (...     op  pack이름 ) : ((E1 op E2) op E3 )
 
+// binary right fold : (pack이름 op   ... op  I )        : (E1 op ( E2 op ( E3 op I) ) )
+// binary right fold : (I        op  ... op  pack 이름 ) : (((I op E1) op E2) op E3) )
 
 template<typename ... Types> 
 int Sum(Types ... args)
 {
-	int s = (args + ...); // (1 + ( 2 + 3) )
+//	int s = (args + ...);     // (1 + ( 2 + 3) )
+
+	int s = (args + ... + 0); // (1 + ( 2 + ( 3 + 0) )
 
 	return s;
 }
+
 int main()
 {
-	int n = Sum(1, 2, 3);
+	int n = Sum();
+//	int n = Sum(1, 2, 3);
 
 	std::cout << n << std::endl;
 }
