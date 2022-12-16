0eebd95240b23c53f44964bddb6a79430ec0796a
ecourse <smkang@codenuri.co.kr>
2022-12-16 15:37:21 +0900

    Add files via upload

--- DAY5/new1.cpp
+++ DAY5/new1.cpp
@@ -11,6 +11,25 @@ struct Point
 
 int main()
 {
-	Point* p1 = new Point(1, 2);
-	delete p1;
+	// new 의 동작 방식
+	// 1. 메모리 할당 - operator new()
+	// 2. 생성자 호출 - 
+//	Point* p1 = new Point(1, 2);
+
+	// delete 동작 방식
+	// 1. 소멸자 호출
+	// 2. 메모리 해지 - operator delete();
+//	delete p1;
+
+	// 1. 메모리만 할당
+	Point* p2 = static_cast<Point*>( operator new(sizeof(Point)) );
+
+	// 2. 메모리 할당없이 생성자만 호출
+	new (p2) Point(1, 2);
+
+	// 3. 메모리 해지 없이 소멸자만 호출
+	p2->~Point();
+
+	// 4. 메모리만 해지
+	operator delete(p2);
 }
