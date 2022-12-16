fc2fd7e33dccf8261005c5e7cb1d97a4050d93b4
ecourse <smkang@codenuri.co.kr>
2022-12-16 14:49:59 +0900

    Add files via upload

--- DAY5/7_thread4.cpp
+++ DAY5/7_thread4.cpp
@@ -7,6 +7,10 @@ using namespace std::literals;
 
 // promise & future
 
+// 스레드를 고려하지 않은 일반 함수라면 반환 값으로 함수 결과를 돌려 줍니다.
+// 그런데 아래 함수를 스레드로 수행후 결과를 꺼내 오려면
+// 1. promise & future
+// 2. async
 int add1(int a, int b)
 {
 	int c = a + b; // 복잡한 연산
