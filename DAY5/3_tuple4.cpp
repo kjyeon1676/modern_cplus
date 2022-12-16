256bed9ab154dec23740380c8ca01e29720264ee
ecourse <smkang@codenuri.co.kr>
2022-12-16 11:21:45 +0900

    Add files via upload

--- DAY5/3_tuple4.cpp
+++ DAY5/3_tuple4.cpp
@@ -8,18 +8,23 @@ struct tuple
 template<typename T, typename ... Types>
 struct tuple<T, Types...> : public tuple< Types...>
 {
+	using base = tuple< Types...>;
+
 	T value;
 
 	tuple() = default;
-	tuple(const T& n) : value(n) {}
+	tuple(const T& n, const Types& ... args ) 
+		: value(n), base(args...) {}
 
-	static constexpr int N = 1;
+	static constexpr int N = base::N + 1;
 };
 
 int main()
 {
-	tuple<> t0;						// 보관 안함 - primary 사용
-	tuple<             short> t1;	// short  보관
-	tuple<     double, short> t2;	// double 보관
-	tuple<int, double, short> t3;	// int 보관
+//	tuple<> t0;						// 보관 안함 - primary 사용
+//	tuple<             short> t1;	// short  보관
+//	tuple<     double, short> t2;	// double 보관
+//	tuple<int, double, short> t3;	// int 보관
+
+	tuple<int, double, short> t3(1, 3.4, 2);
 }
