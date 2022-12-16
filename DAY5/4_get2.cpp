918e050b928217458c6516b637303ac7a6075fb7
ecourse <smkang@codenuri.co.kr>
2022-12-16 11:51:00 +0900

    Add files via upload

--- DAY5/4_get2.cpp
+++ DAY5/4_get2.cpp
@@ -29,11 +29,23 @@ int main()
 
 	std::cout << t3.value << std::endl; // 1
 
-	std::cout << t3.value << std::endl; // 3.4 나오게 해보세요
+	std::cout << static_cast<tuple<double, short>&>(t3).value << std::endl; // 3.4 나오게 해보세요
 
-	std::cout << t3.value << std::endl; // 2 나오게 해보세요.
+	std::cout << static_cast<tuple<short>&>(t3).value << std::endl; // 2 나오게 해보세요.
+
+	// get 을 생각해 봅시다.
+	double d = get<1>(t3); 
 }
-
+template<int N, typename T>
+튜플인 T 타입의 N 번재 요소의 타입&
+get(T& tp)
+{
+	return static_cast<T의 N번째 부모 타입&>(tp).value;
+}
+// 결국 get 을 완성하려면
+// => 임의의 tuple 타입 T의 N번째 요소의 타입 구하기
+// => 임의의 tuple 타입 T의 N번째 기반클래스 타입 구하기
+// 입니다.
 
 
 
